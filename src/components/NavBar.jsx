4a03a3e5370634a5759c694f7047a2709bcc2c22
Corey DeLoach <cdeloach16@gmail.com>
2021-11-21 09:54:27 -0600

    experimenting with react-bootstrap, building cards and navbar with expandable menus for small screens

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,57 +1,40 @@
 import React from 'react';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
-// import Home from '../views/Home';
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 const NavBar = () => {
     return (
         <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-            <Navbar.Brand href="#home">CYF</Navbar.Brand>
+            <Navbar.Brand href="/">
+                <div className="logo-button border-dark">
+                    <div className="logo-1">C</div>
+                    <div className="logo-2">YF</div>
+                </div>
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/about-us">Contact Us</Nav.Link>
-                    <Nav.Link href="/contact-us">About Us</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
+                <Nav>
+                    <Link
+                        className="nav-button border-dark nav-spacing"
+                        to="/mission">who we are</Link>
+                    <Link
+                        className="nav-button border-dark nav-spacing"
+                        to="/research">our research</Link>
+                    <Link
+                        className="nav-button border-dark nav-spacing"
+                        to="/people">people and values</Link>
+                    <Link
+                        className="nav-button border-dark nav-spacing"
+                        to="/news">news</Link>
+                    <Link
+                        className="nav-button border-dark nav-spacing"
+                        to="/resources">resources</Link>
                 </Nav>
+                <div className="d-flex justify-content-center align-items-center mt-2">
+                    <a className="join-button d-flex align-items-center" href="mailto:cyarar@uab.edu">join us</a>
+                </div>
             </Navbar.Collapse>
         </Navbar>
-
-
-        // <div className='navbar navbar-expand-md bg-dark navbar-dark'>
-        //     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapseNavbar">
-        //         <span className="navbar-toggler-icon"></span>
-        //     </button>
-
-        //     <div className="collapse navbar-collapse" id="collapseNavbar">
-        //         <ul className="navbar-nav">
-        //             <li className="nav-item">
-        //                 <a href="/whoweare" className="nav-link">who we are</a>
-        //             </li>
-        //             <li className="nav-item">
-        //                 <a href="/research" className="nav-link">who we are</a>
-        //             </li>
-        //             <li className="nav-item">
-        //                 <a href="/people" className="nav-link">who we are</a>
-        //             </li>
-        //         </ul>
-        //     </div>
-        //     <Link className='logo-button' to='/'>
-        //         <div className="logo-1">C</div>
-        //         <div className="logo-2">YF</div>
-        //     </Link>
-        //     <Link className='nav-button' to='/'>who we are</Link>
-        //     <Link className='nav-button' to='/'>our research</Link>
-        //     <Link className='nav-button' to='/'>people and values</Link>
-        //     <Link className='nav-button' to='/'>news</Link>
-        //     <Link className='nav-button' to='/'>resources</Link>
-        //     <a className="join-button" href="mailto:cyarar@uab.edu">join us</a>
-        // </div>
     )
 }
 
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,22 +1,74 @@
 import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap'
 import RootLayout from '../components/RootLayout';
 
 const Home = () => {
     return (
-        <RootLayout>
-            
-                <div className="card col-md-4 card-header-info m-4 transition">
-                    <h2 className="card-header">A rebel's approach to changing SCI care</h2>
-                    <div className="card-body">
-                        <h4 className="card-text ">Changing acute care and care in the chronic stages of spinal cord injury through using E-Stim, ketogenic dietary strategies, and gut microbiome modifications.</h4>
-                    </div>
-                </div>
-                <div className="card col-md-4 card-header-info m-4 transition">
-                    <div className="background-img">Yo</div>
-                </div>
+        <>
+            <RootLayout>
+                <Col md={4}>
+                    <Card className="bg-dark text-light border-dark rounded p-2 m-2 transition my-5">
+                        <Card.Title className="text-center" style={{ fontSize: 40 }}>A rebel's approach to changing <span className="d-inline" style={{ color: 'rgb(31, 255, 160)' }}>SCI</span> care</Card.Title>
+                        <Card.Body>
+                            <Card.Text style={{ fontSize: 20 }}>
+                                Changing acute care and care in the chronic stages of <span className="d-inline" style={{ color: 'rgb(31, 255, 160)' }}>spinal cord injury</span> through using E-Stim, ketogenic dietary strategies, and gut microbiome modifications.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col md={6}>
+                    <Card className="bg-dark border-dark rounded m-2 p-2 transition">
+                        <Card.Img className="background-img">
 
-        </RootLayout>
-
+                        </Card.Img>
+                    </Card>
+                </Col>
+            </RootLayout>
+            <Container>
+                <Row className="my-5 d-flex align-items-center justify-content-center">
+                    <Col md={6}>
+                        <Card className="bg-dark border-dark text-light rounded shadow m-2 p-2 transition">
+                            <Card.Title className="text-center mt-2" style={{ fontSize: 40 }}>Who we are</Card.Title>
+                            <Card.Body>
+                                <Card.Text>
+                                    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis excepturi commodi praesentium laboriosam! Blanditiis magnam molestias quod expedita esse. Fuga optio, numquam blanditiis beatae maiores sed temporibus assumenda culpa quos.</span>
+                                </Card.Text>
+                            </Card.Body>
+                            <div className="m-5">
+                                <a href="/people" className="join-button m-5">our team</a>
+                            </div>
+                        </Card>
+                    </Col>
+                    <Col md={6}>
+                        <Card className="bg-dark border-dark text-light rounded shadow m-2 p-2 transition">
+                            <Card.Title className="text-center mt-2" style={{ fontSize: 40 }}>We are a team on a mission</Card.Title>
+                            <Card.Body>
+                                <div className="wheelchair-img"></div>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+            <RootLayout>
+                <h1 className="text-center text-light display-4 my-4">Our Research</h1>
+                <Row xs={1} md={2} className="g-4 transition">
+                    {Array.from({ length: 4 }).map((_, idx) => (
+                        <Col key={idx}>
+                            <Card className="bg-dark border-dark text-light rounded shadow m-2 p-2 transition">
+                                <Card.Img variant="top" src="holder.js/100px160" />
+                                <Card.Body>
+                                    <Card.Title>Card title</Card.Title>
+                                    <Card.Text>
+                                        This is a longer card with supporting text below as a natural
+                                        lead-in to additional content. This content is a little bit longer.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            </RootLayout>
+        </>
     )
 }
 
